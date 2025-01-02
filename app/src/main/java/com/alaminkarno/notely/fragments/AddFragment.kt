aa4f079fde0f4b68d0e21b0afd4823b7932ab1b2
Md. Al-Amin <amin15-1951@diu.edu.bd>
2025-01-02 19:42:50 +0600

    feat: add note to room database

--- app/src/main/java/com/alaminkarno/notely/fragments/AddFragment.kt
+++ app/src/main/java/com/alaminkarno/notely/fragments/AddFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.alaminkarno.notely.R
+import com.alaminkarno.notely.model.Note
+import com.alaminkarno.notely.viewmodel.NoteViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class AddFragment : Fragment() {
+
+    private lateinit var controller: NavController
+    private lateinit var viewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +28,34 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        viewModel = ViewModelProvider(this)[NoteViewModel::class.java]
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        controller = Navigation.findNavController(view)
+
+        val title = view.findViewById<EditText>(R.id.add_note_title)
+        val description = view.findViewById<EditText>(R.id.add_note_description)
+        val saveNoteButton = view.findViewById<FloatingActionButton>(R.id.save_note_nav_button)
+
+        saveNoteButton.setOnClickListener {
+            val noteTitle = title.text.trim().toString()
+            val noteDescription = description.text.trim().toString()
+
+            if(noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
+                saveNoteToDatabase(Note(noteTitle,noteDescription))
+                controller.navigate(R.id.action_addFragment_to_mainFragment)
+            } else if(noteTitle.isEmpty()) {
+                title.error = "Title is required"
+            } else if(noteDescription.isEmpty()) {
+                description.error = "Description is required"
+            }
+        }
+    }
+
+    private fun saveNoteToDatabase(note: Note){
+        context?.let { viewModel.insert(it,note) }
     }
 
 }
--- app/src/main/java/com/alaminkarno/notely/fragments/MainFragment.kt
+++ app/src/main/java/com/alaminkarno/notely/fragments/MainFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.alaminkarno.notely.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainFragment : Fragment() {
+
+    lateinit var controller : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,17 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        controller = Navigation.findNavController(view)
+
+        val button = view.findViewById<FloatingActionButton>(R.id.add_note_fav_button)
+
+        button.setOnClickListener {
+            controller.navigate(R.id.action_mainFragment_to_addFragment)
+        }
+
     }
 
 }
